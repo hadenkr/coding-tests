3726b68642d96c456c5d18e285bb192815bac282
hadenkr <hadenkr@gmail.com>
2025-03-24 14:57:36 +0900

    Solved 31403

---
+++ baekjoon/solved-ac-class1/31403.py
@@ -0,0 +1,9 @@
+import sys
+
+s1 = sys.stdin.readline().rstrip()
+s2 = sys.stdin.readline().rstrip()
+s3 = sys.stdin.readline().rstrip()
+n1, n2, n3 = int(s1), int(s2), int(s3)
+
+print(n1+n2-n3)
+print(int(s1 + s2)-n3)
