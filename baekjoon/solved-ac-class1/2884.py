d034004f82704d40715028ded736aafc254530d0
hadenkr <hadenkr@gmail.com>
2025-03-26 11:31:36 +0900

    Remove comments

--- baekjoon/solved-ac-class1/2884.py
+++ baekjoon/solved-ac-class1/2884.py
@@ -2,17 +2,10 @@ import sys
 
 h, m = map(int, sys.stdin.readline().rstrip().split(maxsplit=1))
 
-# if m >= 45:
-#     m -= 45
-# else:
-#     m += 15
-#     h = 23 if h == 0 else h-1
-
-m -= 45
-if m < 0:
-    m += 60
-    h -= 1
-    if h < 0:
-        h = 23
+if m >= 45:
+    m -= 45
+else:
+    m += 15
+    h = 23 if h == 0 else h-1
 
 print(h, m)
